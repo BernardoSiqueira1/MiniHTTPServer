83cb671081495fc81b2f220b1f464d2c203ba77b
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-07-14 22:03:48 -0300

    added response formatter

---
+++ HTTPServer/src/main/java/server/parser/response/ResponseFormatter.java
@@ -0,0 +1,12 @@
+package server.parser.response;
+
+import server.model.response.ClientResponseObject;
+
+public final class ResponseFormatter{
+
+    public String format(ClientResponseObject clientResponseObject){
+        String formattedObject;
+
+        return null;
+    }
+}
