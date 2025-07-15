be548ad6f50773d255bfc680c029b5ad731b273c
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-07-14 22:02:00 -0300

    refactor on httphandlerchain

--- HTTPServer/src/main/java/server/httpchain/HTTPHandlerChain.java
+++ HTTPServer/src/main/java/server/httpchain/HTTPHandlerChain.java
@@ -1,7 +1,45 @@
 package server.httpchain;
 
-public interface HTTPHandlerChain extends Runnable {
+import server.model.response.ClientResponseObject;
+import server.configuration.HandlerChainConfiguration;
+import server.handler.RequestHandler;
+import server.model.request.ClientRequestObject;
+import server.parser.request.RequestParser;
+import server.parser.response.ResponseFormatter;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.List;
+
+public class HTTPHandlerChain implements Runnable {
+
+    private final Socket clientConnection;
+    private final RequestParser requestParser = new RequestParser();
+    private final ResponseFormatter responseFormatter = new ResponseFormatter();
+    private final List<RequestHandler> httpHandlersList;
+
+    public HTTPHandlerChain(HandlerChainConfiguration HandlerChainConfiguration, Socket clientConnection){
+        this.clientConnection = clientConnection;
+        this.httpHandlersList = HandlerChainConfiguration.getRequestHandlerList();
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            ClientRequestObject clientRequestObject = requestParser.parse(clientConnection.getInputStream());
+            ClientResponseObject clientResponseObject = null;
+
+            for (RequestHandler handler: httpHandlersList){
+                handler.handleRequest(clientRequestObject, clientResponseObject);
+            }
 
 
+        }
+        catch (IOException ioException){
+            System.out.println(ioException.getMessage());
+        }
+
+    }
 
 }
