904c93174a8e8f0d1cd1c8611b3b86f21f682aef
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-07-17 17:00:10 -0300

    removed main test methods.

--- HTTPServer/src/main/java/app/MainApplication.java
+++ HTTPServer/src/main/java/app/MainApplication.java
@@ -9,12 +9,6 @@ import java.io.IOException;
 public class MainApplication {
 
     public static void main(String[] args) throws IOException {
-        HandlerChainConfiguration httpChainConfiguration = DefaultHandlerChainConfiguration.getDefaultChainConfiguration();
-        Server HTTPApp = new Server(8080);
-        HTTPApp.setHandlerChainConfiguration(httpChainConfiguration);
-        HTTPApp.setServiceDispatcher(null);
-
-        HTTPApp.listen();
     }
 
 }
