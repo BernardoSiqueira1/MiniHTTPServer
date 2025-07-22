cb45a54da3eae9b253341f783c833935e006d406
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-07-22 20:44:36 -0300

    renamed request processor class

---
+++ HTTPServer/src/main/java/server/httpchain/HTTPRequestPipeline.java
@@ -0,0 +1,45 @@
+package server.httpchain;
+
+import server.iohandlers.response.ResponseSender;
+import server.iohandlers.response.ResponseSerializer;
+import server.model.response.ClientResponseObject;
+import server.model.request.ClientRequestObject;
+import server.iohandlers.request.RequestParser;
+import server.service.ThirdPartyDispatcher;
+
+import java.net.Socket;
+
+public final class HTTPRequestPipeline implements Runnable {
+
+    private final Socket clientConnection;
+    //private final List<RequestHandler> httpHandlersList;
+
+    public HTTPRequestPipeline(Socket clientConnection){
+        this.clientConnection = clientConnection;
+    }
+
+    @Override
+    public void run() {
+
+        ClientRequestObject clientRequestObject = null;
+        ClientResponseObject clientResponseObject = null;
+        String serializedResponse = "";
+
+        try {
+            clientRequestObject = RequestParser.parse(clientConnection.getInputStream());
+
+            clientResponseObject = ThirdPartyDispatcher.callService(clientRequestObject);
+            serializedResponse = ResponseSerializer.format(clientResponseObject);
+
+            ResponseSender.sendToClient(clientConnection, serializedResponse);
+        }
+
+        catch (Exception exception){
+
+            System.out.println(exception.getMessage());
+
+        }
+
+    }
+
+}
